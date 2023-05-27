7bc98284cc34c110c53bff977102c6f8ac29bd52
Karl Wulfert <pic16f877.ccs@emailn.de>
2023-05-27 18:48:19 +0200

    Add feature esc

--- src/txt_frame.rs
+++ src/txt_frame.rs
@@ -4,6 +4,7 @@ use crate::Color;
 use crate::FrameVar;
 use core::iter;
 use smallstr::SmallString;
+use strip_ansi_escapes::strip;
 
 /// The abstract representation of a TextFrame.
 ///
@@ -271,6 +272,98 @@ impl TextFrame {
 
         let lines_buffer_iter = text.lines().flat_map(move |line| {
             let curr_line_len = line.chars().count();
+            let max_line_diff = max_line_len - curr_line_len;
+
+            let alignment = match self.algn {
+                Algn::Left => (sum_exp_width, max_line_diff - sum_exp_width),
+                Algn::Centr => (max_line_diff / 2, max_line_diff - max_line_diff / 2),
+                Algn::Right => (max_line_diff - sum_exp_width, sum_exp_width),
+            };
+
+            let iter_top = iter::once(self.color_fra.into_fg_str())
+                .chain(iter::once(self.vert_left_line.as_str()))
+                .chain(iter::once(self.color_fill.into_fg_str()))
+                .chain(iter::repeat(self.fill.as_str()).take(alignment.0));
+
+            let iter_line = iter::once(self.color_txt.into_fg_str()).chain(iter::once(line));
+
+            let iter_bottom = iter::once(self.color_fill.into_fg_str())
+                .chain(iter::repeat(self.fill.as_str()))
+                .take(alignment.1 + 1)
+                .chain(iter::once(self.color_fra.into_fg_str()))
+                .chain(iter::once(self.vert_right_line.as_str()))
+                .chain(iter::once(Color::default().into_fg_str()))
+                .chain(iter::once("\n"));
+
+            iter_top.chain(iter_line).chain(iter_bottom)
+        });
+
+        top_half_frame_iter
+            .chain(lines_buffer_iter)
+            .chain(bottom_half_frame_iter)
+    }
+
+    #[cfg(feature = "esc")]
+    #[cfg(feature = "color")]
+    #[cfg(feature = "newline")]
+    #[cfg_attr(docsrs, doc(cfg(feature = "newline")))]
+    /// Create an iterator frame with a newline.
+    pub fn frame_iterln_esc<'a>(&'a self, text: &'a str) -> impl Iterator<Item = &str> {
+        self.frame_iter(&text)
+            .chain(iter::once("\n"))
+    }
+
+    #[cfg(feature = "esc")]
+    #[cfg(feature = "color")]
+    /// Create an iterator frame.
+    pub fn frame_iter_esc<'a>(&'a self, text: &'a str) -> impl Iterator<Item = &str> {
+        let (lines, max_line_len) = max_line_len_no_esc(text);
+
+        let sum_exp_width = self.expand_width + self.expand;
+        let max_line_len = if ((max_line_len + 2) + sum_exp_width * 2) < self.width {
+            (self.width - (max_line_len + 2 + sum_exp_width * 2)) + max_line_len
+        } else {
+            max_line_len
+        } + sum_exp_width * 2;
+
+        let sum_exp_height = self.expand + self.expand_height;
+        let sum_lines = sum_exp_height * 2 + 2 + lines;
+        let take_enlarge_top = (max_line_len + 7) * sum_exp_height;
+
+        let take_enlarge_btm = if sum_lines < self.height {
+            take_enlarge_top + (max_line_len + 7) * (self.height - sum_lines)
+        } else {
+            take_enlarge_top
+        };
+
+        let enlarge_line_iter = iter::once(self.color_fra.into_fg_str())
+            .chain(iter::once(self.vert_left_line.as_str()))
+            .chain(iter::once(self.color_fill.into_fg_str()))
+            .chain(iter::repeat(self.fill.as_str()).take(max_line_len))
+            .chain(iter::once(self.color_fra.into_fg_str()))
+            .chain(iter::once(self.vert_right_line.as_str()))
+            .chain(iter::once(Color::default().into_fg_str()))
+            .chain(iter::once("\n"))
+            .cycle();
+
+        let top_half_frame_iter = iter::once(self.color_fra.into_fg_str())
+            .chain(iter::once(self.left_top_cnr.as_str()))
+            .chain(iter::repeat(self.hor_top_line.as_str()).take(max_line_len))
+            .chain(iter::once(self.right_top_cnr.as_str()))
+            .chain(iter::once(Color::default().into_fg_str()))
+            .chain(iter::once("\n"))
+            .chain(enlarge_line_iter.clone().take(take_enlarge_top));
+
+        let bottom_half_frame_iter = enlarge_line_iter
+            .take(take_enlarge_btm)
+            .chain(iter::once(self.color_fra.into_fg_str()))
+            .chain(iter::once(self.left_btm_cnr.as_str()))
+            .chain(iter::repeat(self.hor_btm_line.as_str()).take(max_line_len))
+            .chain(iter::once(self.right_btm_cnr.as_str()))
+            .chain(iter::once(Color::default().into_fg_str()));
+
+        let lines_buffer_iter = text.lines().flat_map(move |line| {
+            let curr_line_len = std::str::from_utf8(&strip(line).unwrap()).unwrap().chars().count();
             let max_line_diff = max_line_len - curr_line_len;
 
             let alignment = match self.algn {
@@ -605,6 +698,12 @@ impl TextFrame {
         self.color_fill = color;
         self
     }
+}
+
+#[cfg(feature = "esc")]
+#[inline]
+fn max_line_len_no_esc(text: &str) -> (usize, usize) {
+    max_line_len(std::str::from_utf8(&strip(text).unwrap()).unwrap())
 }
 
 #[inline]
